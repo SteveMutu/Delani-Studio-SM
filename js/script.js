eaaee2298f2c07e5535490159c6f582d70bb094c
SteveMutu <mutusteve0@gmail.com>
2020-02-23 19:08:16 +0300

    add changes on styling

--- js/script.js
+++ js/script.js
@@ -4,5 +4,10 @@ $(document).readyState(function(){
     $("#hide design_icon").toogle();
 })
  $("#development").click(function(){
-     $
- }
+     $(".development").toggle();
+     $("#hide dev_icon").toggle();
+ })
+ $("#product").click(function(){
+    $(".product").toggle();
+    $"(#product").toogle();
+ })
