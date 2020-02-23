6de4db85c4705c151d0847dc3e1b06ec14546b1f
SteveMutu <mutusteve0@gmail.com>
2020-02-23 18:58:36 +0300

    added styles

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).readyState(function(){
+ $("#design").click(function(){
+    $(".design").toggle();
+    $("#hide design_icon").toogle();
+})
+ $("#development").click(function(){
+     $
+ }
