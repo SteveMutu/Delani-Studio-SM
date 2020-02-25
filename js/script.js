5546df79d2a49f098640c2ff38def187d04d6d58
SteveMutu <mutusteve0@gmail.com>
2020-02-25 08:26:16 +0300

    add class to images

--- js/script.js
+++ js/script.js
@@ -32,4 +32,27 @@ $(document).ready(function(){
     $(".image1").hover(function(){
         $("#t1").toggle()
     })
+    $(".image2").hover(function(){
+        $("#t2").toggle()
+    })
+    $(".image3").hover(function(){
+        $("#t3").toggle()
+    })
+    $(".image4").hover(function(){
+        $("#t4").toggle()
+    })
+    $(".image5").hover(function(){
+        $("#t5").toggle()
+    })
+    $(".image6").hover(function(){
+        $("#t6").toggle()
+    })
+    $(".image7").hover(function(){
+        $("#t7").toggle()
+    })
+    $(".image8").hover(function(){
+        $("#t8").toggle()
+    })
+
+
 });
